3a0baab189120b4b835e1455472b95fe22ddd98f
BreakfastAtKyrils <kyrildr@gmail.com>
2021-12-31 12:30:59 -0500

    login + cookie for username

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -22,19 +24,31 @@ function generateRandomString() {
 
 //this get method simply renders all the URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"],
+ };
   res.render("urls_index", templateVars)
 })
 
 //create a new URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"],
+ };
+  res.render("urls_new", templateVars);
 });
 
 //displays a specific URL page --> use urls_show.ejs
 app.get("/urls/:shortURL", (req, res) => {
   console.log('GET /urls/:shortURL !')
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
+  };
+
   res.render("urls_show", templateVars);
 })
 
@@ -48,6 +62,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//adds new URL
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   const shortURL = generateRandomString();
@@ -60,19 +75,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 });
 
-//this is how we update a URL
+//updates a URL
 app.post("/urls/:id", (req, res) => { 
-  console.log('DB before')
-  console.log(urlDatabase)
   urlDatabase[req.body.short] = req.body.longURL;
-
-  console.log('DB after')
-  console.log(urlDatabase)
-  
   //res.redirect(`/urls/${shortURL}`)
-  
 })
 
+//deletes URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   console.log(req.params)
   delete urlDatabase[req.params.shortURL]
@@ -80,6 +89,19 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   console.log('here is the database after the action was performed:    ')
   console.log(urlDatabase);
 
+  res.redirect("/urls")
+})
+
+app.post("/login", (req, res) => {
+  console.log(req.body.username)
+  res.cookie('username', req.body.username)
+  console.log(res.cookie)
+
+  res.redirect("/urls")
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
   res.redirect("/urls")
 })
 
